274864a829e1eb97dfd39de96e5705d8d12c58b9
AhmedAyman77 <ai388981@gmail.com>
2025-12-21 00:38:06 +0200

    implement arcjet for limit requests and Bot protection

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { login, register, logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import arcjetProtection from "../middleware/archjet.middleware.js";
 
 const authRouter = Router();
 
+authRouter.use(arcjetProtection);
+
+authRouter.get("/test-arcjet", (req, res) => {
+    res.send("Auth Route is working");
+});
 authRouter.post("/login", login);
 authRouter.post("/register", register);
 authRouter.post("/logout", logout);
 authRouter.put("/update-profile", protectRoute, updateProfile);
-authRouter.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+authRouter.get("/get-profile", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default authRouter;
