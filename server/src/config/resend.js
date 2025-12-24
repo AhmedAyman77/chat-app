6fbdd2e8b5ca07a7e8826250eb9ab5774eb05f6b
AhmedAyman77 <ai388981@gmail.com>
2025-12-25 00:45:40 +0200

    debug

--- server/src/config/resend.js
+++ server/src/config/resend.js
@@ -1,7 +1,6 @@
 import { Resend } from "resend";
 import ENV from "./env.js";
 
-console.log("Resend API Key:", ENV.RESEND_EMAIL_API_KEY); // Debugging line
 export const resend = new Resend(ENV.RESEND_EMAIL_API_KEY);
 
 export const sender = {
